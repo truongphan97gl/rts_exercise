d1955fb5bf286d526b1f2bf13ec66c3bca6ebac4
Truong <truongphan97gl@gmail.com>
2021-05-04 20:48:33 -0400

    first package name

--- src/com/rts/Main.java
+++ src/com/rts/Main.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.rts;
 //#1 Print the number of integers in an array that are above the given input and the number that are below,
 // e.g. for the array [1, 5, 2, 1, 10] with input 6, print “above: 1, below: 4”.
 //
@@ -36,7 +36,7 @@ public class Main {
     public static void main(String[] args) {
 //        int[] arr = {1,5,2,1,10};
 //        question1(arr,6);
-        question2("MyString", 7);
+        question2("MyString", 2);
 
     }
 }
